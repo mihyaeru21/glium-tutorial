7b16aeb7c8b358f7e0008e8e10f262a0bc587660
Mihyaeru <mihyaeru21@gmail.com>
2019-06-15 15:11:32 +0900

    :sparkles: create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    use glium::glutin;
+
+    let mut events_loop = glutin::EventsLoop::new();
+    let wb = glutin::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let _display = glium::Display::new(wb, cb, &events_loop).unwrap();
+
+    let mut closed = false;
+    while !closed {
+        events_loop.poll_events(|ev| match ev {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::CloseRequested => closed = true,
+                _ => (),
+            },
+            _ => (),
+        });
+    }
 }
