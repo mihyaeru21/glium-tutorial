edc71d94133807b6afbbfa4c1d64ddb8bdad1c04
Mihyaeru <mihyaeru21@gmail.com>
2019-06-15 15:22:50 +0900

    :sparkles: draw background color

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use glium::Surface;
+
 fn main() {
     use glium::glutin;
 
     let mut events_loop = glutin::EventsLoop::new();
     let wb = glutin::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
-    let _display = glium::Display::new(wb, cb, &events_loop).unwrap();
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
 
     let mut closed = false;
     while !closed {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
         events_loop.poll_events(|ev| match ev {
             glutin::Event::WindowEvent { event, .. } => match event {
                 glutin::WindowEvent::CloseRequested => closed = true,
